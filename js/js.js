ffe45922d1435339fcefda3595b691f983077b26
zynp <mizraklizeynep@gmail.com>
2023-02-18 20:44:38 +0300

    yasam

---
+++ js/js.js
@@ -0,0 +1,7 @@
+function yazi_duzenle(){
+document.getElementById('demo').style.fontSize='50px';
+document.getElementById('demo').style.textAlign='center';
+document.getElementById('demo').style.textDecoration='underline';
+document.getElementById('demo').style.textDecoration='underline';
+
+}
